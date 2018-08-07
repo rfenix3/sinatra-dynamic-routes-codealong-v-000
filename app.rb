37dd58a5e545db03913f91f16861ffb20f5e2ce4
Richard Fenix <richard.fenix@gmail.com>
2018-08-07 18:33:53 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -24,9 +24,4 @@ class App < Sinatra::Base
     "Answer is #{@answer}"
   end
 
-  # get "/multiply/:num1/:num2" do
-  #   @product = params[:num1].to_i * params[:num2].to_i
-  #   "#{@product}"
-  # end
-
 end
